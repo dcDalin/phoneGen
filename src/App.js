8eb551cb0f856af3104ec1893ce1a3b77524942c
dcDalin <mcdalinoluoch@gmail.com>
2019-08-26 18:00:10 +0300

    Setup travis

--- src/App.js
+++ src/App.js
@@ -1,83 +1,145 @@
 import React from "react";
+import phoneNumberGenerator from "./utils";
 import IconAscending from "./assets/icons/numeric-ascending-sort.svg";
 import IconDescending from "./assets/icons/numerical-descending-sort.svg";
 
-function App() {
-  return (
-    <div className="w-1/2 mx-auto ">
-      <h1 className="p-10 text-center text-4xl font-semibold text-gray-100">
-        Random Phone Number Generator
-      </h1>
-      <div class="flex p-4">
-        <div class="w-1/2 mr-4 h-12">
-          <div className="mb-4 p-4">
-            <h1 className="text-gray-200 text-lg font-bold mb-2">
-              Total Generated Phonenumbers
-            </h1>
-            <div className="">
-              <p className="font-mono text-6xl font-extrabold text-gray-100">
-                23
-              </p>
+class App extends React.Component {
+  state = {
+    amount: null,
+    numbers: [],
+    error: null,
+    asc: false
+  };
+  componentDidMount() {
+    const numbers = JSON.parse(localStorage.getItem("numbers"));
+    /* istanbul ignore next */
+    if (numbers && numbers.length > 0) {
+      /* istanbul ignore next */
+      this.setState({ numbers });
+    }
+  }
+
+  /* istanbul ignore next */
+  generateNumbers = e => {
+    e.preventDefault();
+    const { amount } = this.state;
+    const numbers = phoneNumberGenerator(amount);
+    this.setState({ numbers });
+    localStorage.setItem("numbers", JSON.stringify(numbers));
+  };
+
+  /* istanbul ignore next */
+  sortAsc = e => {
+    e.preventDefault();
+    const { numbers } = this.state;
+
+    this.setState({ numbers: numbers.sort() });
+  };
+
+  sortDesc = e => {
+    e.preventDefault();
+    const { numbers } = this.state;
+
+    this.setState({ numbers: numbers.reverse() });
+  };
+
+  handleChange = e => {
+    this.setState({ amount: e.target.value });
+  };
+
+  /* istanbul ignore next */
+  handleSort = e => {
+    e.preventDefault();
+    const { asc, numbers } = this.state;
+
+    this.setState({ asc: !asc });
+
+    if (!asc) {
+      this.setState({ numbers: numbers.sort() });
+    } else {
+      this.setState({ numbers: numbers.reverse() });
+    }
+  };
+  render() {
+    const { numbers, error, amount } = this.state;
+    const disable = amount > 10000 || amount < 1;
+    return (
+      <div className="w-1/2 mx-auto ">
+        <h1 className="p-10 text-center text-4xl font-semibold text-gray-100">
+          Random Phone Number Generator
+        </h1>
+        <div className="flex p-4">
+          <div className="w-1/2 mr-4 h-12">
+            <div className="mb-4 p-4">
+              <h1 className="text-gray-200 text-lg font-bold mb-2">
+                Total Generated Phonenumbers
+              </h1>
+              <div className="">
+                <p className="font-mono text-6xl font-extrabold text-gray-100">
+                  {this.state.amount}
+                </p>
+              </div>
             </div>
           </div>
+          <form className="w-1/2 ml-4 bg-gray-600">
+            <div className="p-4">
+              <label
+                className="block text-gray-200 text-sm font-bold mb-2"
+                htmlFor="phoneNumber"
+              >
+                How many phonenumbers?
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="number"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="flex px-4 pb-4 items-center justify-between">
+              <button
+                className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                onClick={this.generateNumbers}
+                disabled={disable}
+              >
+                Generate
+              </button>
+            </div>
+          </form>
         </div>
-        <div class="w-1/2 ml-4 bg-gray-600">
-          <div class="p-4">
-            <label
-              class="block text-gray-200 text-sm font-bold mb-2"
-              for="username"
-            >
-              How many phonenumbers?
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="number"
-            />
-          </div>
-          <div class="flex px-4 pb-4 items-center justify-between">
-            <button
-              class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Generate
+        <div className="flex-1 ml-8 mr-4 p-4 bg-gray-600">
+          <div className="w-full mb-2">
+            <p className="text-lg font-bold text-gray-100">
+              All Generated Phone Numbers
+            </p>
+            <button className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+              <img
+                className="h-4"
+                src={IconAscending}
+                alt="Sort Ascending"
+                onClick={this.sortAsc}
+              />
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+              <img
+                className="h-4"
+                src={IconDescending}
+                alt="Sort Descending"
+                onClick={this.sortDesc}
+              />
             </button>
           </div>
+          <div className="w-full bg-gray-200 p-4">
+            <ul className="font-mono font-semibold list-inside tracking-wide text-gray-600">
+              {numbers &&
+                numbers.map((phone, idx) => <li key={idx}>{phone}</li>)}
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="flex ml-8 mr-4 mb-4 p-4 bg-gray-600">
-        <div class="w-1/2 h-12">
-          <p className="font-small text-gray-100">Max Generated Phone Number</p>
-          <p className="text-lg font-bold text-gray-300">0737273773</p>
-        </div>
-        <div class="w-1/2  h-12">
-          <p className="font-small text-gray-100">Min Generated Phone Number</p>
-          <p className="text-lg font-bold text-gray-300">0737273773</p>
-        </div>
-      </div>
-      <div class="flex-1 ml-8 mr-4 p-4 bg-gray-600">
-        <div className="w-full mb-2">
-          <p className="text-lg font-bold text-gray-100">
-            All Generated Phone Numbers
-          </p>
-          <button class="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-            <img className="h-4" src={IconAscending} alt="Sort Ascending" />
-          </button>
-          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-            <img className="h-4" src={IconDescending} alt="Sort Descending" />
-          </button>
-        </div>
-        <div className="w-full bg-gray-200 p-4">
-          <ul className="font-mono font-semibold list-inside tracking-wide text-gray-600">
-            <li>0712345673</li>
-            <li>0982837838</li>
-            <li>0712345673</li>
-            <li>0982837838</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+export default howMany => {
+  const numbers = [];
+
+  if (howMany > 0 && howMany < 10001) {
+    /* istanbul ignore next */
+    for (let i = 0; i < howMany; i++) {
+      const rand = Math.ceil(Math.random() * 100000000) + "";
+      if (rand.length < 8) {
+        numbers.push("07" + rand + Math.floor(Math.random() * 10));
+      } else if (rand.length > 8) {
+        numbers.push("07" + rand.slice(0, -1));
+      } else {
+        numbers.push("07" + rand);
+      }
+    }
+
+    return numbers;
+  }
+};
